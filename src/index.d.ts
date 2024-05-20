b139d9d5518274e7a8801ad0c5cedca86cdf64fd
Kenny Daniel <platypii@gmail.com>
2024-05-20 02:04:40 -0700

    Don't depend on hyparquet for Compressors type

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,15 @@
-import type { Compressors } from 'hyparquet'
+export type CompressionCodec =
+  'UNCOMPRESSED' |
+  'SNAPPY' |
+  'GZIP' |
+  'LZO' |
+  'BROTLI' |
+  'LZ4' |
+  'ZSTD' |
+  'LZ4_RAW'
+
+export type Compressors = {
+  [K in CompressionCodec]?: (input: Uint8Array, outputLength: number) => Uint8Array
+}
 
 export const compressors: Compressors
