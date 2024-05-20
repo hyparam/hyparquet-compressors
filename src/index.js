915f0254d3595ddc4dcff99381339c5303506e90
Kenny Daniel <platypii@gmail.com>
2024-05-19 17:14:16 -0700

    Native lz4

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { snappyUncompressor } from 'hysnappy'
-import lz4 from 'lz4'
 import pako from 'pako'
+import { LZ4 } from './lz4.js'
 
 /**
  * @type {import('hyparquet').Compressors}
@@ -10,9 +10,5 @@ export const compressors = {
   GZIP: input => pako.ungzip(input),
   BROTLI: () => new Uint8Array(), // TODO
   ZSTD: () => new Uint8Array(), // TODO
-  LZ4: (input, outputLength) => {
-    const out = Buffer.alloc(outputLength)
-    lz4.decodeBlock(Buffer.from(input), out)
-    return out
-  },
+  LZ4,
 }
---
+++ src/lz4.js
@@ -0,0 +1,39 @@
+/**
+ * LZ4 decompression
+ *
+ * @param {Uint8Array} input
+ * @param {number} outputLength
+ * @returns {Uint8Array}
+ */
+export function LZ4(input, outputLength) {
+  const output = new Uint8Array(outputLength)
+  let len = 0 // output position
+  for (let i = 0; i < input.length;) {
+    const token = input[i++]
+
+    let literals = token >> 4
+    if (literals) {
+      // literal length
+      let byte = literals + 240
+      while (byte === 255) literals += byte = input[i++]
+      // copy literals
+      output.set(input.subarray(i, i + literals), len)
+      len += literals
+      i += literals
+      if (i >= input.length) return output
+    }
+
+    const offset = input[i++] | input[i++] << 8
+    if (!offset || offset > len) throw new Error('lz4 offset out of range')
+    // match length
+    let matchLength = (token & 0xf) + 4
+    let byte = matchLength + 240
+    while (byte === 255) matchLength += byte = input[i++]
+    // copy match
+    let pos = len - offset
+    const end = len + matchLength
+    while (len < end) output[len++] = output[pos++]
+  }
+
+  return output
+}
